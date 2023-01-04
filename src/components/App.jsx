36fb2f17f2e09775303f75cc4af3812d10afe2c0
Vitalii Blyskun <97634483+VitaliiBlyskun@users.noreply.github.com>
2023-01-04 16:12:30 +0200

    add homework for review

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,7 @@
 import { lazy } from 'react';
-import { Routes, Route } from 'react-router-dom';
-// import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { SharedLayout } from './SharedLayout';
 
-// import Home from 'pages/Home';
-// import Movies from 'pages/Movies';
-// import Reviews from './Review/Review';
-// import Cast from './Cast/Cast';
-// import MovieDetails from 'pages/MovieDetails';
 
 const Home = lazy(() => import('pages/Home'));
 const Movies = lazy(() => import('pages/Movies'));
@@ -27,7 +21,7 @@ export const App = () => {
           <Route path="reviews" element={<Reviews />} />
         </Route>
       </Route>
-      {/* <Route path="*" element={<Navigate to="/" replace />} /> */}
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 };
